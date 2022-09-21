5876572e45d187e0c67b597ced683d7498fbfe73
lagon <lagon@DESKTOP-IVS9RIG>
2022-09-21 13:36:33 +0100

    Kata1 versión 3

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,18 +1,12 @@
 package kata1;
-
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
+	
+import java.time.LocalDate;
+	
 public class Kata1 {
-
-    public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(2002, 5, 26);
-        
+	    
+    public static void main(String[] args){
+        LocalDate date = LocalDate.of(2002, 5, 23);
         Person person = new Person("Alba", date);
-        System.out.println(person.getName() + " tiene " + person.getAge()
-                + " años");
+        System.out.println(person.getName() + " tiene " + person.getAge() + " años.");
     }
-    
 }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,33 +1,28 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
     private final String name;
-    private final Calendar birthdate;
-    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
-
-    public Person(String name, Calendar birthdate) {
+    private final LocalDate birthdate;
+    
+    public Person(String name, LocalDate birthdate){
         this.name = name;
         this.birthdate = birthdate;
     }
-
-    public String getName() {
+    
+    public String getName(){
         return name;
     }
-
-    public Calendar getBirthdate() {
+    
+    public LocalDate getBirthdate(){
         return birthdate;
     }
     
     public int getAge(){
-        Calendar today = GregorianCalendar.getInstance();
-        return (int) (milliSecondsToYear(today.getTimeInMillis() - birthdate.getTimeInMillis()));
+        LocalDate today = LocalDate.now();
+        return Period.between(birthdate,today).getYears();    
     }
-    
-    private long milliSecondsToYear(long milles){
-        return milles / MILLISECONDS_PER_YEAR;
-    }
+
 }
