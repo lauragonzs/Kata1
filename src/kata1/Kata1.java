d61b7c0bfdf7dca0c0a6af7d0b67050c6f7a312c
lagon <lagon@DESKTOP-IVS9RIG>
2022-09-21 13:30:49 +0100

    Kata1 versión 2

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,12 +1,18 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Alba", new Date(102,5,26));
-        System.out.println(person.getName() + " tiene " + person.getAge());
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2002, 5, 26);
+        
+        Person person = new Person("Alba", date);
+        System.out.println(person.getName() + " tiene " + person.getAge()
+                + " años");
     }
     
 }
